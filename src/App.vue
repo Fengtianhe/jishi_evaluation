0824ebcf5afee787323bb30f274d6df23dc5b367
jing-yue <xyyspace@163.com>
2018-06-13 14:36:03 +0800

    加360统计代码

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,7 @@
     }
   }
 
+  // 百度统计
   var _hmt = _hmt || [];
   (function() {
       var hm = document.createElement("script");
@@ -30,6 +31,11 @@
       var s = document.getElementsByTagName("script")[0];
       s.parentNode.insertBefore(hm, s);
   })();
+  //360统计
+  (function(b,a,e,h,f,c,g,s){b[h]=b[h]||function(){(b[h].c=b[h].c||[]).push(arguments)};
+      b[h].s=!!c;g=a.getElementsByTagName(e)[0];s=a.createElement(e);
+      s.src="//s.union.360.cn/"+f+".js";s.defer=!0;s.async=!0;g.parentNode.insertBefore(s,g)
+  })(window,document,"script","_qha",263959,false);
 
 </script>
 
